9fb6dcfa1f8b82d122bd2c432f0887b790160430
lingoaaa <dol-renat@mail.ru>
2023-06-22 20:02:09 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+# for sym in s:
+#     count = 0
+#     for sub_sym in s:
+#         if sub_sym == sym:
+#             count +=1
+#     print(sym, count)
+# s = 'aaabca'
+# syms_counter = {}
+# for sym in s:
+#     syms_counter[sym] = syms_counter.get(sym,0 )+1
+# print(syms_counter)
+
