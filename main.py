f52c3c410b449244fb0dd422352fecf7087289c9
lingoaaa <dol-renat@mail.ru>
2023-06-22 20:54:09 +0400

    изменения 1

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-
-
-# for sym in s:
-#     count = 0
-#     for sub_sym in s:
-#         if sub_sym == sym:
-#             count +=1
-#     print(sym, count)
-# s = 'aaabca'
-# syms_counter = {}
-# for sym in s:
-#     syms_counter[sym] = syms_counter.get(sym,0 )+1
-# print(syms_counter)
-
+pol = input('Введите слово:')
+polindrom=True
+if pol[::-1]==pol:
+    print(polindrom)
+else:
+    polindrom=False
+    print(polindrom)
